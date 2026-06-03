18a27041d31007be77f1e2341811c7873f1f877a
Aviralansh <avirald204@gmail.com>
2026-06-03 14:32:41 +0530

    [ADD] Pre-setup done

---
+++ logging_middleware/logger.js
@@ -0,0 +1,34 @@
+require("dotenv").config();
+
+const TOKEN = process.env.LOGGING_AUTH_TOKEN;
+const LOR_URL = process.env.LOR_URL;
+
+async function Log(stack, level, pkg, message) {
+    try {
+        const response = await fetch(LOG_URL, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+
+                "Authorization": `Bearer ${TOKEN}`
+            },
+            body: JSON.stringify({
+                stack,
+                level,
+                package: pkg,
+                message
+            })
+        });
+
+        const data = await response.json();
+
+        return data;
+    }
+    catch (err) {
+        
+        console.error("Logging failed:", err.message);
+        return null;
+    }
+}
+
+module.exports = Log;
